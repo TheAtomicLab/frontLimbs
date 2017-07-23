dac06990eb31d0c903e93a8bb811414b6ed28ff1
MatiasGF <matiasngf@hotmail.com>
2017-07-23 16:03:12 -0300

    registro
    html y css para seleccionar la clase de usuario

---
+++ registro2.php
@@ -0,0 +1,8 @@
+<?php include('include/header.php'); ?>
+
+<article id="reg2">
+	<a href=""><div class="selec"><div class="fondo"></div><h2>Quiero pedir una mano</h2></div></a>
+	<a href=""><div class="selec"><div class="fondo"></div><h2>Tengo impresora y quiero ayudar</h2></div></a>
+</article>
+
+<?php include('include/footer.php'); ?>
